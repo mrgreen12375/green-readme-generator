f6054fb7a6f38e7527537753acac956835e832f6
Steven Green <ste2144105@gmail.com>
2022-10-01 15:25:01 -0700

    add questions array

--- index.js
+++ index.js
@@ -1,7 +1,49 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'Whats the title of you project?',
+        name: 'title'
+    },
+    {
+        type: 'input',
+        message: 'Enter description of your project',
+        name: 'description'
+    },
+    {
+        type: 'input',
+        message: 'Enter the installation script',
+        name: 'installation'
+    },
+    {
+        type: 'input',
+        message: 'Enter the usage',
+        name: 'usage'
+    },
+    {
+        type: 'list',
+        message: 'Select license',
+        name: 'license',
+        choices: ['none', 'Apache', 'GNU', 'MIT']
+    },
+    {
+        type: 'input',
+        message: 'Enter the contributors (seperate by commas)',
+        name: 'contributing'
+    },
+    {
+        type: 'input',
+        message: 'Enter the test script',
+        name: 'test'
+    },
+    {
+        type: 'input',
+        message: 'Enter the Frequently asked questions',
+        name: 'faq'
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
@@ -11,3 +53,30 @@ function init() {}
 
 // Function call to initialize app
 init();
+
+
+const inquirer = require('inquirer');
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      message: 'What is your user name?',
+      name: 'username',
+    },
+    {
+      type: 'password',
+      message: 'What is your password?',
+      name: 'password',
+    },
+    {
+      type: 'password',
+      message: 'Re-enter password to confirm:',
+      name: 'confirm',
+    },
+  ])
+  .then((response) =>
+    response.confirm === response.password
+      ? console.log('Success!')
+      : console.log('You forgot your password already?!')
+  );
